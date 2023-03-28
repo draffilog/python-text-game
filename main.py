741e829c9661f2b975a11eeb72e81f8a466c3b90
Draffilog <karkushov@gmail.com>
2023-03-28 18:38:31 +0100

    I've separated files for now with GUI and main program for now , later in my updates I will implement it together

--- main.py
+++ main.py
@@ -1,62 +1,49 @@
-import tkinter
-import random
+class Player:
+    """
+    This class represents the player character in the game including their name, money, health, points, and inventory.
+    """
 
-main_window = tkinter.Tk()
-main_window.title("Text-based adventure game")
-main_window.geometry("400x400")
-name = input("Enter your name please >> ")
-money = random.randint(50,310)
-point = 0
-health = 100
-top_frame = tkinter.Frame(main_window)
-top_frame.pack(anchor = "n")
+class Room:
+    """
+    This class represents a room in the game, containing a description, enemy, points, items, money, and treasure.
+    """
 
-name_label = tkinter.Label(top_frame, text ="Name: "+ name, font = ("Calibri, 16"))
-name_label.grid(row = 0, column = 0)
-health_label = tkinter.Label(top_frame, text = "Health: "+str(health), font = ("Calibri, 16"))
-health_label.grid(row = 1, column = 0)
-money_label = tkinter.Label(top_frame, text = "Money: "+str(money), font = ("Calibri, 16"))
-money_label.grid(row = 0, column = 1)
-point_label = tkinter.Label(top_frame, text = "Point: "+str(point), font = ("Calibri, 16"))
-point_label.grid(row = 1, column = 1)
+class Item:
+    """
+    This class represents a generic item in the game that can be used by the player.
+    """
 
-def change_labels(money, health, point):
-    money_label.configure(text = "Money: "+str(money))
-    health_label.configure(text = "Health: "+str(health))
-    point_label.configure(text = "Point: "+str(point))
+class Weapon(Item):
+    """
+    This class represents a weapon in the game, a subclass of Item, with a name, damage, and price.
+    """
 
+class Key(Item):
+    """
+    This class represents a key in the game, a subclass of Item, with a code (0 or 1) and price.
+    """
 
-bottom_frame = tkinter.Frame(main_window)
-bottom_frame.pack(anchor = "s")
+class Armour(Item):
+    """
+    This class represents an armour in the game, a subclass of Item, with durability and price.
+    """
 
-def Room1():
-    print("Hello, you are in room 1")
-    point = 10
-    change_labels(money, health, point)
-    pass
-def Room2():
-    pass
-def Room3():
-    pass
-def Room4():
-    pass
-def Shop():
-    pass
-def Inventory():
-    pass
+class HealingPad(Item):
+    """
+    This class represents a HealingPad in the game, a subclass of Item, which can be bought to increase the player's health by 50.
+    """
 
-R1_btn = tkinter.Button(bottom_frame, text="Room 1", command = Room1)
-R1_btn.grid(row=0, column=0)
-R2_btn = tkinter.Button(bottom_frame, text="Room 2", command = Room2)
-R2_btn.grid(row=0, column=1)
-R3_btn = tkinter.Button(bottom_frame, text="Room 3", command = Room3)
-R3_btn.grid(row=1, column=0)
-R4_btn = tkinter.Button(bottom_frame, text="Room 4", command = Room4)
-R4_btn.grid(row=1, column=1)
-In_btn = tkinter.Button(bottom_frame, text = "Inventory", command = Inventory)
-In_btn.grid(row=0, column=2)
-Sh_btn = tkinter.Button(bottom_frame, text="Shop", command = Shop)
-Sh_btn.grid(row=1, column=2)
+class Enemy:
+    """
+    This class represents an enemy in the game including their name damage, and health.
+    """
 
+class Game:
+    """
+    This class represents the main game, which manages the game loop, user interface, and game logic.
+    """
 
-main_window.mainloop()
+class FileManager:
+    """
+    This class handles reading and writing information from text files.
+    """
